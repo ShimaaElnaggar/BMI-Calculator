63371d493e0e59f9fa4dc559c15a9ff7f678066b
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-01-01 17:58:09 +0200

    create BMI view appBar

--- lib/views/bmi_view.dart
+++ lib/views/bmi_view.dart
@@ -6,6 +6,21 @@ class BMIView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+            child: Text(
+              "BMI CALCULATOR",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
